9ae19d9b2019f2abedf6aa565e3b01805a8293c3
khs <69028828+happysang@users.noreply.github.com>
2022-01-12 01:19:00 +0900

    first commit

--- mainpage/models.py
+++ mainpage/models.py
@@ -10,8 +10,8 @@ class Type(models.Model):
 
 # Seokgeun - 작품 모델 생성 12.21
 class Work(models.Model):
-    title = models.CharField(max_length = 50) # 작품 제목
-    artist = models.CharField(max_length= 50) # 창작자  창작자 여럿이라 외래키 뺌
+    title = models.CharField(max_length = 500) # 작품 제목
+    artist = models.CharField(max_length= 500) # 창작자  창작자 여럿이라 외래키 뺌
 
     wtype = models.ForeignKey(Type, null=True, on_delete=models.CASCADE)  # 작품 타입 (Graphic, Advertise, Media)
 
@@ -19,11 +19,11 @@ class Work(models.Model):
 
     # thumbnail = models.ImageField(null = True, upload_to="%Y/%m/%d")  # 작품 썸네일
     # pic = models.ImageField(null = True, upload_to="%Y/%m/%d")  # 작품 사진파일
-    thumbnail = models.CharField(max_length=50)
-    pic = models.CharField(max_length=50)
+    thumbnail = models.CharField(max_length=500)
+    pic = models.CharField(max_length=500)
 
     # picprd = models.ImageField(null = True, blank = True, upload_to="%Y/%m/%d")  # 그래픽 전용 - 연출사진파일
-    picprd = models.CharField(max_length=50)
+    picprd = models.CharField(max_length=500)
     youtube = models.CharField(blank = True, null = True, max_length= 200) # 미디어 전용 - 유튜브 링크
 
 
@@ -38,6 +38,6 @@ class Profile(models.Model):
     d_pimage = models.CharField(max_length=500,blank=True, null=True)
     eng_name = models.CharField(max_length=500,blank=True, null=True)
     introduce = models.TextField(max_length=500,blank=True, null=True)
-    email = models.CharField(max_length=20,blank=True, null=True)
+    email = models.CharField(max_length=500,blank=True, null=True)
     wpage = models.CharField(max_length=500,blank=True, null=True)
     project = models.ForeignKey(Work, on_delete = models.CASCADE,blank=True, null=True)
